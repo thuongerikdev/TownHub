0442541a4c2e9aa7201f42a8fe897d7d04821882
Nguyễn Xuân Thưởng <121345028+thuongerikdev@users.noreply.github.com>
2026-06-14 11:07:38 +0700

    add ocrEngine to OcrJob

---
+++ TH.WebAPI/TH.WebAPI/Migrations/AssetDb/20260614040708_AddOcrEngine.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TH.WebAPI.Migrations.AssetDb
+{
+    /// <inheritdoc />
+    public partial class AddOcrEngine : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ocrEngine",
+                schema: "asset",
+                table: "ocr_jobs",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ocrEngine",
+                schema: "asset",
+                table: "ocr_jobs");
+        }
+    }
+}
